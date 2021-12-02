420c62ac2bef4a817440d39bbe643f427e1155f9
youngoo <1965214573@qq.com>
2021-12-02 09:08:35 +0800

    归并算法

---
+++ sort/src/Code03MergeSort.java
@@ -0,0 +1,40 @@
+/**
+ * @author youngoo
+ * @date 2021/12/2 8:36
+ */
+public class Code03MergeSort {
+
+    public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length - 1);
+    }
+
+    public static void process(int[] arr, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        process(arr, l, mid);
+        process(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    public static void merge(int[] arr, int l, int m, int r) {
+        int[] help = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = m + 1;
+        while (p1 <= m && p2 <= r) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= m) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        System.arraycopy(help, 0, arr, l, help.length);
+    }
+}
