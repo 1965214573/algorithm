5ed370d88b5376e430466a000200665eb3700114
youngoo <1965214573@qq.com>
2021-12-06 21:28:59 +0800

    topk算法

--- sort/src/Code07SortArrayDistanceLessK.java
+++ sort/src/Code07SortArrayDistanceLessK.java
@@ -9,10 +9,15 @@ public class Code07SortArrayDistanceLessK {
    public static void sortedArrDistanceLessK(int[] arr, int k) {
        PriorityQueue<Integer> heap = new PriorityQueue<>();
        int index = 0;
-       int i = 0;
        // 初始化小根堆的前k个数据
        for (; index < Math.min(arr.length, k); index++) {
            heap.add(arr[index]);
+       }
+
+       int i = 0;
+       for (; index < arr.length; i++, index++) {
+           heap.add(arr[index]);
+           arr[i] = heap.poll();
        }
        while (!heap.isEmpty()) {
            arr[i++] = heap.poll();
